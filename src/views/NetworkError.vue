c885ead9f0eea31b8ae67568447921d45c632c13
shahrokh <shahrokhelmi@gmail.com>
2022-04-08 18:49:22 +0430

    L9-end

---
+++ src/views/NetworkError.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="networkError">
+    <h1>uh-oh!</h1>
+    <h3>Network Issues.....</h3>
+    <a href="#" @click="$router.go(-1)">go back</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NetworkError"
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/event/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Oops!</h1>
+    <h3>The {{ resource }} does not found.</h3>
+    <router-link :to="{ name: 'EventList' }">Back to home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  props: {
+    resource: {
+      type: String,
+      required: true,
+      default: "page"
+    }
+  }
+};
+</script>
+
+<style scoped></style>
